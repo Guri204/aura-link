8ed4b430d4d051b08ddb3334474989954853126a
gurjashan singh <jashanmoga2004@gmail.com>
2024-12-13 14:41:51 -0600

    index finished

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,24 @@
 'use strict';
 
-// Predefined credentials in localStorage
 localStorage.setItem("username", "jaysn@email.com");
 localStorage.setItem("password", "sukha204");
 
-// Add event listener to login button
 const loginBtn = document.getElementById('loginButton');
 loginBtn.addEventListener('click', login);
 
-function login() {
-    // Retrieve stored credentials
+function login(event) {
+    event.preventDefault();
+
     const storedUsername = localStorage.getItem('username');
     const storedPassword = localStorage.getItem('password');
 
-    // Get user input
-    const username = document.getElementById('email-phone').value.trim();
+    const username = document.getElementById('user-inputs').value.trim();
     const password = document.getElementById('password').value.trim();
 
-    // Validate credentials
     if (username === storedUsername && password === storedPassword) {
-        // Redirect to home page on successful login
         window.location.href = 'main.html';
     } else {
-        // Show error message on incorrect credentials
-        document.getElementById('message-showing').innerText = 'Incorrect username and/or password';
+        document.querySelector('#message-showing p').classList.add('visible');
+        document.querySelector('#message-showing p').innerText = 'Incorrect username and/or password';
     }
 }
